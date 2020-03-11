f0484a965123bdd8c16635e3dd4d17a53e973721
jl0up <41552872+jl0up@users.noreply.github.com>
2020-03-11 12:36:55 +0100

    Packaged modules properly
    - to install the package (once and for all) in develop mode (run from upper directory):
    `pip install -e instruments`
    - to import a module (example with yoko750 driver):
    `from instruments import yoko750`
    - then use as:
    `y = yoko750.Yoko750(...)`

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="instruments", # Replace with your own username
+    version="0.1",
+    author="Jean-Loup Smirr",
+    author_email="jean-loup.smirr@college-de-france.fr",
+    description="Python interface for VISA instruments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FluxQuantumLab/instruments",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
